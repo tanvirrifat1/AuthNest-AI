96c0a557f36b17efc72f0072f79f14eb6f914d2d
tanvirrifat1 <rifatkhan5567790@gmail.com>
2025-03-11 19:45:13 +0600

    added room

---
+++ src/app/modules/room/room.interface.ts
@@ -0,0 +1,6 @@
+import { Types } from 'mongoose';
+
+export type IRoom = {
+  roomName?: string;
+  user: Types.ObjectId;
+};
---
+++ src/app/modules/room/room.model.ts
@@ -0,0 +1,20 @@
+import { model, Schema } from 'mongoose';
+import { IRoom } from './room.interface';
+
+const questionSchema = new Schema<IRoom>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    roomName: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Room = model<IRoom>('Room', questionSchema);
